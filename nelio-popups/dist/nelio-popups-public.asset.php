36ebce2fc85a76b34637a8d06616d54ff861c36f
Sean Murphy <sean@iamseanmurphy.com>
2025-05-03 00:36:42 +0000

    1.2.6

---
+++ nelio-popups/dist/nelio-popups-public.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-dom-ready', 'wp-hooks', 'wp-i18n'), 'version' => '01ab053ac5410a1a8dd5');
---
+++ nelio-popups/includes/frontend/index.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Nelio_Popups\Frontend;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}//end if
+
+require_once untrailingslashit( __DIR__ ) . '/public.php';
+require_once untrailingslashit( __DIR__ ) . '/loaders.php';
